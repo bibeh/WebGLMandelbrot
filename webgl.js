0cc75f4578157615fbdf2c9862ebe386411e1942
Rafael Reyes <bibeh1979@gmail.com>
2015-09-11 11:56:18 +0800

    Basic running example, mostly from http://webglfundamentals.org/

---
+++ webgl.js
@@ -0,0 +1,35 @@
+var gl = document.getElementById("canvas").getContext("experimental-webgl");
+
+function createShader(id, type) {
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, document.getElementById(id).text);
+    gl.compileShader(shader);
+    return shader;
+}
+
+var program = gl.createProgram();
+gl.attachShader(program, createShader("2d-vertex-shader", gl.VERTEX_SHADER));
+gl.attachShader(program, createShader("2d-fragment-shader", gl.FRAGMENT_SHADER));
+gl.linkProgram(program);
+gl.useProgram(program);
+
+// Create a buffer and put a single clipspace rectangle in it (2 triangles)
+gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([
+        -1.0, -1.0,
+         1.0, -1.0,
+        -1.0,  1.0,
+        -1.0,  1.0,
+         1.0, -1.0,
+         1.0,  1.0]),
+    gl.STATIC_DRAW);
+
+// look up where the vertex data needs to go.
+var positionLocation = gl.getAttribLocation(program, "a_position");
+gl.enableVertexAttribArray(positionLocation);
+gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+
+// draw
+gl.drawArrays(gl.TRIANGLES, 0, 6);
