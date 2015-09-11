046d02f490acc2564de659477d428dccd189fcf1
Rafael Reyes <bibeh1979@gmail.com>
2015-09-11 20:28:27 +0800

    And back to straight code

--- webgl.js
+++ webgl.js
@@ -1,17 +1,15 @@
-function p(x, f) { return f(x), x; }
-
 var gl = document.getElementById("canvas").getContext("experimental-webgl");
 
-var program = p(gl.createProgram(), program => {
-    [ "2d-vertex-shader", "2d-fragment-shader" ].forEach((id) => {
-        gl.attachShader(program, p(gl.createShader(id.indexOf("vertex") > 0 ? gl.VERTEX_SHADER : gl.FRAGMENT_SHADER), shader => {
-            gl.shaderSource(shader, document.getElementById(id).text);
-            gl.compileShader(shader);
-        }));
-    });
-    gl.linkProgram(program);
-    gl.useProgram(program);
-});
+var program = gl.createProgram();
+[ {id: "2d-vertex-shader"  , type: gl.VERTEX_SHADER  },
+  {id: "2d-fragment-shader", type: gl.FRAGMENT_SHADER} ].forEach(i => {
+    var shader = gl.createShader(i.type);
+    gl.shaderSource(shader, document.getElementById(i.id).text);
+    gl.compileShader(shader);
+    gl.attachShader(program, shader);
+})
+gl.linkProgram(program);
+gl.useProgram(program);
 
 // Create a buffer and put a single clipspace rectangle in it (2 triangles)
 gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
@@ -31,5 +29,4 @@ var positionLocation = gl.getAttribLocation(program, "a_position");
 gl.enableVertexAttribArray(positionLocation);
 gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
 
-// draw
 gl.drawArrays(gl.TRIANGLES, 0, 6);
